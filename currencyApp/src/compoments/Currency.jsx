bade3353446f35afa70015d585e70ccde4224376
Taner <tanerr.ask@gmail.com>
2024-05-20 00:59:48 +0300

    Initial commit

---
+++ currencyApp/src/compoments/Currency.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import '../css/currency.css'
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import axios from 'axios';
+
+let BASE_URL = "https://api.freecurrencyapi.com/v1/latest";
+let API_KEY = "fca_live_QNIfyHvadfBQ1MguD7TewXbQnYrbC9y6ycUFzrY1";
+
+
+function Currency() {
+
+  const[amount, setAmount] = useState(0);
+  const[fromCurrency, setFromCurrency] = useState('USD');
+  const[toCurrency, setToCurrency] = useState('TRY');
+  const[result, setResult] = useState(0);
+
+  const exchange = async () => {
+    const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`);
+    const result = ((response.data.data[toCurrency]) * amount).toFixed(2);
+    setResult(result);
+  }
+
+  return (
+    <div className='currency-div'>
+
+      <div className='title'>
+        <h3>DÖVİZ HESAPLAMA</h3>
+      </div>
+
+      <div style={{marginTop: '25px'}}>
+      <input 
+      value={amount} onChange={(e) => setAmount(e.target.value)}
+      type="number" className='amount'/>
+
+        <select 
+        onChange={(e) => setFromCurrency(e.target.value)}
+        className='from-currency-option'>
+            <option>USD</option>
+            <option>EUR</option>
+            <option>TRY</option>
+        </select>
+
+        <FaRegArrowAltCircleRight className='right-allow'/>
+
+        <select 
+        onChange={(e) => setToCurrency(e.target.value)}
+        className='to-currency-option'>
+            <option>TRY</option>
+            <option>EUR</option>
+            <option>USD</option>
+        </select>
+
+        <input 
+        value={result}
+        type="number" className='result'/>
+
+      </div>
+
+      <div>
+        <button 
+        onClick={exchange}
+        className='exchange-button'>Çevir</button>
+      </div>
+       
+    </div>
+  )
+}
+
+export default Currency
